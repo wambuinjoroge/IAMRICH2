98ed8d4fc16ebdaac944fda175e9fbbacd39d43d
Yvonne ariga <yvonnekemi@hotmail.com>
2019-05-17 09:33:20 +0300

    created an emoji dictionary

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by MobiDev on 16/05/2019.
+//  Copyright © 2019 MobiDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+//    @IBAction func showMessage(sender:UIButton){
+//        let alertController = UIAlertController(title: "Welcome to my first app!", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+//        alertController.addAction(UIAlertAction(title:"Ok",style:UIAlertAction.Style.default,handler:nil))
+//        present(alertController,animated:true,completion: nil)
+//
+//}
+    @IBAction func showMessage(sender:UIButton){
+        
+        var emojiDict:[String:String] = ["👻":"Ghost","😀":"Smiley","🙀":"Scream","😎":"Cool"]
+        
+        let selectedButton = sender
+        
+        if let wordToLookUp = selectedButton.titleLabel?.text{
+            var meaning = emojiDict[wordToLookUp]
+       
+        
+        let alertController = UIAlertController(title: "Meaning", message: meaning, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title:"Ok",style:UIAlertAction.Style.default,handler:nil))
+        present(alertController,animated:true,completion: nil)
+        
+    }
+        
+    }
+}
+
