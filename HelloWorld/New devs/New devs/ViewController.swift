651ea5411235fa84156846db037b274229540440
Yvonne ariga <yvonnekemi@hotmail.com>
2019-06-11 09:33:13 +0300

    added a new autolayout scheme

---
+++ HelloWorld/New devs/New devs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  New devs
+//
+//  Created by MobiDev on 11/06/2019.
+//  Copyright © 2019 MobiDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
